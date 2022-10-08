b26b7087b0275d6cdaa12e7e3cea93c802de0ec9
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-10-08 18:09:07 +0530

    add basic packages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,39 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { useState } from 'react'
+import './App.css'
+import { Greet } from '../wailsjs/go/main/App'
+import { MantineProvider, Text } from '@mantine/core'
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen/HomeScreen'
+import MainScreen from './screens/MainScreen/MainScreen'
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+  const [resultText, setResultText] = useState(
+    'Please enter your name below 👇'
+  )
+  const [name, setName] = useState('')
+  const updateName = (e: any) => setName(e.target.value)
+  const updateResultText = (result: string) => setResultText(result)
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+  function greet() {
+    Greet(name).then(updateResultText)
+  }
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-    )
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  )
 }
 
 export default App
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainScreen />,
+  },
+  {
+    path: 'home',
+    element: <HomeScreen />,
+  },
+])
